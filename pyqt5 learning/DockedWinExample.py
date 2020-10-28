c9ccf2d75083ba679681a9b674465788dff0b649
Dan Bai <35791246+milier00@users.noreply.github.com>
2020-10-29 06:53:15 +0800

    Add files via upload

---
+++ pyqt5 learning/DockedWinExample.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'DockedWinExample.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QThread ,  pyqtSignal,  QDateTime
+from PyQt5.QtWidgets import QApplication,  QDialog,  QLineEdit
+import time
+import sys
+
+class BackendThread(QThread):
+    # 通过类成员对象定义信号对象
+    update_date = pyqtSignal(str)
+
+    # 处理要做的业务逻辑
+    def run(self):
+        while True:
+            data = QDateTime.currentDateTime()
+            currTime = data.toString("yyyy-MM-dd hh:mm:ss")
+            self.update_date.emit(str(currTime))
+            time.sleep(1)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(407, 383)
+        MainWindow.setFocusPolicy(QtCore.Qt.ClickFocus)
+        MainWindow.setDockNestingEnabled(False)
+        MainWindow.setUnifiedTitleAndToolBarOnMac(True)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setObjectName("widget")
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lineEdit = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.widget, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 407, 22))
+        self.menubar.setObjectName("menubar")
+        self.menufile = QtWidgets.QMenu(self.menubar)
+        self.menufile.setObjectName("menufile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.dockWidget2 = QtWidgets.QDockWidget(MainWindow)
+        self.dockWidget2.setFloating(True)
+        self.dockWidget2.setObjectName("dockWidget2")
+        self.dockWidgetContents_2 = QtWidgets.QWidget()
+        self.dockWidgetContents_2.setObjectName("dockWidgetContents_2")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.dockWidgetContents_2)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        spacerItem = QtWidgets.QSpacerItem(13, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem)
+        self.verticalLayout_6 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_6.setObjectName("verticalLayout_6")
+        spacerItem1 = QtWidgets.QSpacerItem(20, 18, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_6.addItem(spacerItem1)
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.checkBox = QtWidgets.QCheckBox(self.dockWidgetContents_2)
+        self.checkBox.setObjectName("checkBox")
+        self.verticalLayout_3.addWidget(self.checkBox)
+        self.radioButton = QtWidgets.QRadioButton(self.dockWidgetContents_2)
+        self.radioButton.setObjectName("radioButton")
+        self.verticalLayout_3.addWidget(self.radioButton)
+        self.radioButton_2 = QtWidgets.QRadioButton(self.dockWidgetContents_2)
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.verticalLayout_3.addWidget(self.radioButton_2)
+        self.radioButton_3 = QtWidgets.QRadioButton(self.dockWidgetContents_2)
+        self.radioButton_3.setObjectName("radioButton_3")
+        self.verticalLayout_3.addWidget(self.radioButton_3)
+        self.verticalLayout_5.addLayout(self.verticalLayout_3)
+        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem2)
+        self.verticalLayout_6.addLayout(self.verticalLayout_5)
+        self.horizontalLayout_3.addLayout(self.verticalLayout_6)
+        spacerItem3 = QtWidgets.QSpacerItem(17, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem3)
+        self.gridLayout_3.addLayout(self.horizontalLayout_3, 0, 0, 1, 1)
+        self.dockWidget2.setWidget(self.dockWidgetContents_2)
+        MainWindow.addDockWidget(QtCore.Qt.DockWidgetArea(1), self.dockWidget2)
+        self.dockWidget = QtWidgets.QDockWidget(MainWindow)
+        self.dockWidget.setFloating(True)
+        self.dockWidget.setWindowTitle("")
+        self.dockWidget.setObjectName("dockWidget")
+        self.dockWidgetContents_3 = QtWidgets.QWidget()
+        self.dockWidgetContents_3.setObjectName("dockWidgetContents_3")
+        self.gridLayout_4 = QtWidgets.QGridLayout(self.dockWidgetContents_3)
+        self.gridLayout_4.setObjectName("gridLayout_4")
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        spacerItem4 = QtWidgets.QSpacerItem(13, 17, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem4)
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        spacerItem5 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem5)
+        self.pushButton_5 = QtWidgets.QPushButton(self.dockWidgetContents_3)
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.verticalLayout_4.addWidget(self.pushButton_5)
+        spacerItem6 = QtWidgets.QSpacerItem(20, 33, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem6)
+        self.horizontalLayout_4.addLayout(self.verticalLayout_4)
+        spacerItem7 = QtWidgets.QSpacerItem(13, 17, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem7)
+        self.gridLayout_4.addLayout(self.horizontalLayout_4, 0, 0, 1, 1)
+        self.dockWidget.setWidget(self.dockWidgetContents_3)
+        MainWindow.addDockWidget(QtCore.Qt.DockWidgetArea(1), self.dockWidget)
+        self.actionnew = QtWidgets.QAction(MainWindow)
+        self.actionnew.setObjectName("actionnew")
+        self.actionopen = QtWidgets.QAction(MainWindow)
+        self.actionopen.setObjectName("actionopen")
+        self.menufile.addAction(self.actionnew)
+        self.menufile.addAction(self.actionopen)
+        self.menufile.addSeparator()
+        self.menubar.addAction(self.menufile.menuAction())
+        # 创建线程
+        self.backend = BackendThread()
+        # 连接信号
+        self.backend.update_date.connect(self.handleDisplay)
+        # 开始线程
+        self.backend.start()
+
+    # 将当前时间输出到文本框
+    def handleDisplay(self, data):
+        self.lineEdit.setText(data)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton_5.clicked.connect(self.radioButton.hide)
+        self.pushButton_5.clicked.connect(self.radioButton_2.hide)
+        self.pushButton_5.clicked.connect(self.radioButton_3.hide)
+        self.pushButton_5.clicked.connect(self.checkBox.animateClick)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        self.menufile.setTitle(_translate("MainWindow", "file"))
+        self.checkBox.setText(_translate("MainWindow", "Bias OFF"))
+        self.radioButton.setText(_translate("MainWindow", "10"))
+        self.radioButton_2.setText(_translate("MainWindow", "1"))
+        self.radioButton_3.setText(_translate("MainWindow", "0.1"))
+        self.pushButton_5.setText(_translate("MainWindow", "scan"))
+        self.actionnew.setText(_translate("MainWindow", "new"))
+        self.actionopen.setText(_translate("MainWindow", "open"))
+import figs_rc
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
