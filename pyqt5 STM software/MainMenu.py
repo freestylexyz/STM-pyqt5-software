93969590f5952de001b1a7a2e52544375378e9af
Dan Bai <danbai0328@gmail.com>
2020-11-15 11:18:31 +0800

    change names again and again

--- pyqt5 STM software/MainMenu.py
+++ pyqt5 STM software/MainMenu.py
@@ -1,13 +1,69 @@
 import logging
 import sys
 sys.path.append("./ui/")
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtCore, QtGui, QtWidgets
 # make the example runnable without the need to install
 from os.path import abspath, dirname
 sys.path.insert(0, abspath(dirname(abspath(__file__)) + '/..'))
 
 import qdarkstyle
-import MainMenu_ui as mainwin
+from MainMenu_ui import Ui_HoGroupSTM
+from Setting import mySetting
+from TipApproach_ui import Ui_TipApproach
+from Etest_ui import Ui_ElectronicTest
+
+class myMainMenu(QtWidgets.QMainWindow, Ui_HoGroupSTM):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+        self.actionOpen_Dialog = QtWidgets.QAction()
+        self.actionOpen_Dialog.setObjectName("actionOpen_Dialog")
+        self.actionOpen_tipappr_Dialog = QtWidgets.QAction()
+        self.actionOpen_tipappr_Dialog.setObjectName("actionOpen_tipappr_Dialog")
+        self.actionElectronic_Test = QtWidgets.QAction()
+        self.actionElectronic_Test.setObjectName("actionElectronic_Test")
+
+        self.menuSetting.addAction(self.actionOpen_Dialog)
+        self.menuTip_Approach.addAction(self.actionOpen_tipappr_Dialog)
+        self.menuTest.addAction(self.actionElectronic_Test)
+
+        self.retranslateui()
+        self.actionOpen_Dialog.triggered['bool'].connect(self.opensettingwin)
+        self.actionOpen_tipappr_Dialog.triggered['bool'].connect(self.opentipapprwin)
+        self.actionElectronic_Test.triggered['bool'].connect(self.openelectronictest)
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateui(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.actionOpen_Dialog.setText(_translate("HoGroupSTM","Open Dialog"))
+        self.actionOpen_tipappr_Dialog.setText(_translate("HoGroupSTM", "Open Dialog"))
+        self.actionElectronic_Test.setText(_translate("HoGroupSTM", "Electronic Test"))
+        # call out sub windows from menu
+
+        # Setting subwindow
+        # self.settingwidget = QtWidgets.QWidget()
+        # self.settingwin = Ui_Setting()
+        # self.settingwin.setupUi(self.settingwidget)
+        self.settingwidget = mySetting()
+        # Tip approach subwindow
+        self.tipapprwidget = QtWidgets.QWidget()
+        self.tipapprwin = Ui_TipApproach()
+        self.tipapprwin.setupUi(self.tipapprwidget)
+        # Electronic test subwindow
+        self.electrotestwidget = QtWidgets.QWidget()
+        self.electrotestwin = Ui_ElectronicTest()
+        self.electrotestwin.setupUi(self.electrotestwidget)
+
+    # Open dialog from menubar
+    def opensettingwin(self):
+        self.settingwidget.show()
+    def opentipapprwin(self):
+        self.tipapprwidget.show()
+    def openelectronictest(self):
+        self.electrotestwidget.show()
+
 
 
 def main():
@@ -17,35 +73,10 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
     # create the application and the main window
     app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-
-    # setup ui
-    ui = mainwin.Ui_HoGroupSTM()
-    ui.setupUi(window)
-
-
-    # ui.bt_delay_popup.addActions([
-    #     ui.actionAction,
-    #     ui.actionAction_C
-    # ])
-    # ui.bt_instant_popup.addActions([
-    #     ui.actionAction,
-    #     ui.actionAction_C
-    # ])
-    # ui.bt_menu_button_popup.addActions([
-    #     ui.actionAction,
-    #     ui.actionAction_C
-    # ])
-    #item = QtWidgets.QTableWidgetItem("Test")
-    #item.setCheckState(QtCore.Qt.Checked)
-    #ui.tableWidget.setItem(0, 0, item)
-    #window.setWindowTitle("QDarkStyle example")
-
-    # tabify dock widgets to show bug #6
-   # window.tabifyDockWidget(ui.Bias, ui.Current,ui.Zcontrol)
+    window = myMainMenu()
 
     # setup stylesheet
-#    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 
     # auto quit after 2s when testing on travis-ci
     if "--travis" in sys.argv:
@@ -58,3 +89,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
---
+++ pyqt5 STM software/Setting.py
@@ -0,0 +1,77 @@
+import sys
+sys.path.append("./ui/")
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import serial
+import serial.tools.list_ports
+from time import sleep
+from Setting_ui import Ui_Setting
+
+
+class mySetting(QtWidgets.QWidget, Ui_Setting):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # load settings
+        self.settings = QtCore.QSettings("config.ini", QtCore.QSettings.IniFormat)
+        # self.com = self.settings.value("SETUP/COM_VALUE")
+        self.baud = self.settings.value("SETUP/BAUD_VALUE")#, 0, type=int)
+
+        # init gui
+        #self.comboBox_uart.addItem(self.com)
+        self.comboBox_baud.setCurrentText(self.baud)
+        self.comboBox_baud.currentIndexChanged.connect(self.combox_baud_cb)
+        self.saveButton.clicked.connect(self.btn_test_cb)
+
+
+        def check_valid_uart():
+            # com select
+            port_values = []
+
+            port_list = list(serial.tools.list_ports.comports())
+            length = len(port_list)
+            if (0 == len(port_list)):
+                print("can't find serial port")
+                self.comboBox_uart.setCurrentIndex(-1)
+                self.comboBox_uart.clear()
+            else:
+                for i in range(0, len(port_list)):
+                    port_values.append(port_list[i][0])
+
+                for i in range(len(port_list)):
+                    index = self.comboBox_uart.findText(port_values[i], QtCore.Qt.MatchFixedString)
+                    if (index < 0):
+                        current_text = self.comboBox_uart.currentText()
+                        self.comboBox_uart.addItem(port_values[i])
+                        if ("" == current_text):
+                            # print("current text is NULL")
+                            sleep(0.04)
+                            self.comboBox_uart.setCurrentIndex(-1)
+                        else:
+                            print("current text is ", current_text)
+                # remove eut
+                count = self.comboBox_uart.count()
+                for i in range(count):
+                    data = self.comboBox_uart.itemText(i)
+                    is_uart_exist = 0
+                    for j in range(length):
+                        if (data == port_values[j]):
+                            is_uart_exist = 1
+                    if (0 == is_uart_exist):
+                        # remove1
+                        self.comboBox_uart.removeItem(i)
+                        current_data = self.comboBox_uart.currentText()
+                        if (current_data == data or current_data == ""):
+                            self.comboBox_uart.clear()
+        # find COM
+        check_valid_uart()
+
+    def combox_baud_cb(self):
+            self.baud = self.comboBox_baud.currentText()
+
+    def btn_test_cb(self):
+            self.settings.setValue("SETUP/BAUD_VALUE", self.baud)
+            QtWidgets.QMessageBox.information(self, "Reminder", "QSettings saved successfully!")
