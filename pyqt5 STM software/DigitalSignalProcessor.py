6df1e56d17fe8023497ae0ce4b2269498df14220
freestylexyz <35230526+freestylexyz@users.noreply.github.com>
2020-11-20 14:11:33 -0800

    Update DigitalSignalProcessor.py

--- pyqt5 STM software/DigitalSignalProcessor.py
+++ pyqt5 STM software/DigitalSignalProcessor.py
@@ -5,10 +5,11 @@ Created on Sat Nov 14 18:35:18 2020
 @author: yaoji
 """
 from serial import *
+import time
 from PyQt5.QtCore import *
 
 class myDSP(QObject):
-    succeed_signal = pyqtSignal(bool)       # Serial port open signal
+    succeed_signal = pyqtSignal(bool)          # Serial port open signal
     
     def __init__(self, parent=None, port='com1', baudrate=38400):
         super(DSP, self).__init__(parent, port, baud)
@@ -19,8 +20,8 @@ class myDSP(QObject):
             self.__flush()                  # Flush input and output
             self.version()                  # Update DSP version number
             self.status()                   # Update DSP status information
-            self.succeed = True
-            
+            # Set succeed flag
+            self.succeed = self.open and self.version_obtained and self.status_obtained
         except:
             self.open = False               # Flag that indicates if com port successfully open
             self.idling = True              # Flag that indicates if dsp in an idling state
@@ -28,17 +29,20 @@ class myDSP(QObject):
             self.lastdac = [0x8000] * 16    # Last ouput of all DAC channels
             self.dacrange = [10] * 16       # All DAC channels' current range
             self.adcrange = [0] * 8         # All ADC channels' current range
-            self.last20bit = 0              # Last ouput of 20bit DAC
+            self.last20bit = 0x80000        # Last ouput of 20bit DAC
             self.lastdigital = [False] * 6  # 0 - 5 : bias dither, z dither, feedback, retract, coarse, translation
             self.lastgain = [2] * 4         # 0 -> gain 10.0, 2 -> gain 1.0, 3 -> gain 0.1
             self.offset = [0] * 16          # 0 - 14 are bias offset for different range, 15 is Iset offset
+            
+            self.version_obtained = False
+            self.status_obtained = False
             self.succeed = False            # Flag that indicates if dsp successfully intialized
             
         self.succeed_signal.emit(self.succeed)    # Emit succeed signal of finding dsp
     
     # Check if it is ok to execute a dsp operation        
     def ok(self):
-        return self.succeed & self.idling
+        return self.succeed and self.idling
 
     # Read serial data, proceted method, internal use only
     def __read(self, n):
@@ -66,17 +70,46 @@ class myDSP(QObject):
     # !!! Not finished
     # Acquire DSP version number
     def version(self):
-        if self.open & self.idling:
-            self.ver = 'x'
+        if self.open and self.idling:
+            self.idling = False
+            # self.__write()
+            time.sleep(0.5)       # Sleep 0.5s to wait for dsp data transmission
+            if True:        # Check if receiving enough characters
+                self.ver = 'x'  # read version number
+                self.version_obtained = True
+            else:
+                self.version_obtained = False    # Toggle succeed flag to false if fetching version not succeed
+                self.__flush()
+            self.idling = True
+        else:
+            self.version_obtained = False
     
     # !!! Not finished
     # Acquire DSP status information
     def status(self):
-        if self.open & self.idling:
-            self.lastdac = [0x8000] * 16
-            self.dacrange = [10] * 16
-            self.adcrange = [0] * 8
-            self.last20bit = 0
-            self.lastdigital = [False] * 6  # 0 - 5 : bias dither, z dither, feedback, retract, coarse, translation
-            self.lastgain = [2] * 4         # 0 -> gain 10.0, 2 -> gain 1.0, 3 -> gain 0.1
-            self.offset = [0] * 16          # 0 - 14 are bias offset for different range, 15 is Iset offset
+        if self.open and self.idling:
+            self.idling = False
+            # self.__write()
+            time.sleep(1)       # Sleep 0.5s to wait for dsp data transmission
+            if True:
+                self.lastdac = [0x8000] * 16
+                self.dacrange = [10] * 16
+                self.adcrange = [0] * 8
+                self.last20bit = 0x80000
+                self.lastdigital = [False] * 6  # 0 - 5 : bias dither, z dither, feedback, retract, coarse, translation
+                self.lastgain = [2] * 4         # 0 -> gain 10.0, 2 -> gain 1.0, 3 -> gain 0.1
+                self.offset = [0] * 16          # 0 - 14 are bias offset for different range, 15 is Iset offset
+                self.status_obtained = True
+            else:
+                self.status_obtained = False    # Toggle succeed flag to false if fetching version not succeed
+                self.__flush()
+            self.idling = True
+        else:
+            self.status_obtained = False
+            
+            
+            
+            
+            
+            
+            
