2762008502b509a560e86824c96335dae32e5506
Dan Bai <danbai0328@gmail.com>
2020-11-22 22:56:12 +0800

    setting init added

--- pyqt5 STM software/Setting.py
+++ pyqt5 STM software/Setting.py
@@ -8,8 +8,8 @@
 import sys
 sys.path.append("./ui/")
 from PyQt5.QtWidgets import QApplication , QWidget, QMessageBox
-from PyQt5.QtCore import pyqtSignal , Qt
-from PyQt5.QtCore import Qt , QSettings
+from PyQt5.QtCore import pyqtSignal , QSettings
+from PyQt5 import QtCore
 from Setting_ui import Ui_Setting
 import serial
 import serial.tools.list_ports
@@ -22,25 +22,38 @@ class mySetting(QWidget, Ui_Setting):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        # self.init_UI()
 
+    # auto find available ports and add them into comboBox list
     def find_port(self):
-        port_values = []
-        port_list = list(serial.tools.list_ports.comports())
-        pass
+        port_list = [port[0] for port in serial.tools.list_ports.comports()]
+        # print(port_list)
+        self.comboBox_uart.clear()
+        self.comboBox_uart.addItems(port_list)
 
+    # not called for now
     def init_UI(self):
         self.settings = QSettings("config.ini", QSettings.IniFormat)
         self.baud = self.settings.value("SETUP/BAUD_VALUE")
         self.comboBox_baud.setCurrentText(self.baud)
-        self.comboBox_baud.currentIndexChanged.connect(self.combox_baud_cb)
-        self.loadButton.clicked.connect(self.btn_test_cb)
+        # self.comboBox_baud.currentIndexChanged.connect(self.combox_baud_cb)
+        # self.loadButton.clicked.connect(self.btn_test_cb)
 
+    # pop up a dialog to show dsp_succeed_signal
+    def popdialog(self, ifsucceed):
+        if ifsucceed:
+            QMessageBox.information(self, "Reminder", "DSP initialized successfully!")
+        else:
+            QMessageBox.information(self, "Reminder", "DSP initialization failed!")
+
+    # not called for now
     def combox_baud_cb(self):
-            self.baud = self.comboBox_baud.currentText()
+        self.baud = self.comboBox_baud.currentText()
 
+    # not called for now
     def btn_test_cb(self):
-            self.settings.setValue("SETUP/BAUD_VALUE", self.baud)
-            QMessageBox.information(self, "Reminder", "QSettings loaded successfully!")
+        self.settings.setValue("SETUP/BAUD_VALUE", self.baud)
+        QMessageBox.information(self, "Reminder", "QSettings loaded successfully!")
 
 
 
