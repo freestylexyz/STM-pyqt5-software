598394f3899aabf5364b52b60daa5b8e0a568ed3
freestylexyz <35230526+freestylexyz@users.noreply.github.com>
2020-11-22 17:35:45 -0800

    Update structure example.py

--- pyqt5 learning/structure example.py
+++ pyqt5 learning/structure example.py
@@ -32,6 +32,8 @@ class myMainMenu(QMainWindow, Ui_HoGroupSTM):
         
         #### Connect all signal after instantiation
         self.dsp.succeed_signal.connect(self.dsp_succeed_slot)                   # dsp initialed --> popup messagebox displaying result
+        self.setting.initdsp_signal.connect(self.setting_initdsp_slot)
+        self.setting.loadoffest_signal.connect(self.setting_loadoffest_slot)
 
         #### Load setting
         self.load_config()
@@ -42,23 +44,27 @@ class myMainMenu(QMainWindow, Ui_HoGroupSTM):
     def load_config(self): 
         self.dsp.port = 
         self.dsp.baudrate =
+        self.preampgain =
         # ...... (we need to load all status variable from configuration file in the future)
 
     def open_setting(self):
-        self.seting.init_setting(many varaibles)
+        self.setting.init_setting(many varaibles)
         self.setting.show()
         # self.settingwidget.show()
         
     # pop up a dialog to show dsp_succeed_signal
     def dsp_succeed_slot(self, ifsucceed):
+        self.versionlabel = 
         if ifsucceed:
             QMessageBox.information(self, "Reminder", "DSP initialized successfully!")
         else:
             QMessageBox.information(self, "Reminder", "DSP initialization failed!")
     
-    def setting_initdsp_slot(self):
+    def setting_initdsp_slot(self, port, baudrate):
+        self.dsp.port = port
+        self.dsp.baudrate = baudrate
         self.dsp.init_dsp()     # reinitial dsp
-        self.setting            # change setting view
+        self.init_setting()            # change setting view
     
     def setting_loadoffset_slot(self):
         pass
@@ -75,20 +81,20 @@ class myMainMenu(QMainWindow, Ui_HoGroupSTM):
 
 
 class mySetting(QWidget, Ui_Setting):
-    initdsp = pyqtSignal()
-    loadoffset = pyqtSignal()
+    initdsp = pyqtSignal(str, int)
+    loadoffset = pyqtSignal(int, int, int, int)
 
 
     def __init__(self):                 # Only do most basic initialization say connect signal
         super().__init__()
         self.setupUi(self)
-        # self.init_UI()
+        self.init_UI()
         
     
     #     # not called for now
     def init_UI(self):
-        self.initButton.clicked.connect(self.dsp.init_dsp)
-        self.loadButton.clicked.connect(self.dsp.init_dsp)
+        self.initButton.clicked.connect(self.initButton_slot)
+        self.loadButton.clicked.connect(self.loadButton_slot)
         # self.settings = QSettings("config.ini", QSettings.IniFormat)
         # self.baud = self.settings.value("SETUP/BAUD_VALUE")
         # self.comboBox_baud.setCurrentText(self.baud)
@@ -121,8 +127,6 @@ class mySetting(QWidget, Ui_Setting):
             self.settingwidget.find_port()                                      # ask setting to auto find available ports
     
     
-    def enabled(self):
-        pass
 
 
     # auto find available ports and add them into comboBox list
