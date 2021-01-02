01c6ebdaedfe4b73eb1e7986bd65bd6947432e63
freestylexyz <35230526+freestylexyz@users.noreply.github.com>
2021-01-01 22:50:44 -0800

    update

--- pyqt5 STM software/Scan/Deposition.py
+++ pyqt5 STM software/Scan/Deposition.py
@@ -16,20 +16,35 @@ sys.path.append("../Etest/")
 from PyQt5.QtWidgets import QApplication , QWidget
 from PyQt5.QtCore import pyqtSignal , Qt
 from Deposition_ui import Ui_Deposition
+from sequence import mySequence
 
 
 class myDeposition(QWidget, Ui_Deposition):
     close_signal = pyqtSignal()
+    seq_list_signal = pyqtSignal()
+    do_it_signal = pyqtSignal()
+    stop_signal = pyqtSignal()
+    
 
     def __init__(self):
         super().__init__()
-        # self.setupUi(self)
+        self.setupUi(self)
+        self.init_UI()
 
     def init_UI(self):
-        pass
+        self.seq = mySequence([], [], False)
+        self.bias_dac = False
+        self.bias_ran = 9
+        self.poke_command = [0x42, 0x63, 0x8d]                                  # Feedback, Shift Z offset, Aout bias
+        self.poke_data = [0x00000000, 0x80000000, 0x00008000]                   # OFF, Forward 0 bit, output zero voltage
+        self.poke_seq = mySequence(self.poke_command, self.poke_data, False)    # Initial poke sequence
     
-    def init_deposition(self):
-        pass
+    # Called with int_scan
+    def init_deposition(self, bias_dac, bias_ran):
+        self.bias_dac = bias_dac
+        self.bias_ran = bias_ran
+    
+    # def 
     
     # Emit close signal
     def closeEvent(self, event):
