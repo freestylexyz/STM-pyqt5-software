f6c45c57129551cbf60f2a600444f42784ab9d03
freestylexyz <35230526+freestylexyz@users.noreply.github.com>
2021-01-03 20:37:12 -0800

    update

--- pyqt5 STM software/Model/DataStruct.py
+++ pyqt5 STM software/Model/DataStruct.py
@@ -6,23 +6,34 @@ Created on Sun Jan  3 15:55:44 2021
 """
 from datetime import datetime
 
-
-class dep_data():
+class STMData():
     def __init__(self):
         now = datetime.now()
         self.time = now.strftime("%d/%m/%Y %H:%M:%S")
+        self.seq_name = 'Default'
+        self.path = ''
+        self.bias_dac = False
+        self.preamp_gain = 9
+        self.bias_ran = 9
+        self.setpoint = [0x8000] * 8    #[Bias, Iset, Zoff, Zoff_fine, Xoff, Xin, Yoff, Yin]
+        self.gain = [1.0] * 4           #[Xgain, Ygain, Zgain_1, Zgain_2]
+        self.digital = [False] * 4      #[Dither0, Dither1, Feedback, Retract]
+
+class DepData(STMData):
+    def __init__(self):
+        super().__init__()
         self.data = []
         self.seq_name = 'Poke Default'
-        self.read_mode = 0
+        self.read = [0xdc] + ([0] * 7) #[read_ch, read_mode, read_delay, read_delay2, read_num, read_avg, read_change, read_stop_num]
         
-class image_data():
+class ImageData(STMData):
     def __init(self):
-        now = datetime.now()
-        self.time = now.strftime("%d/%m/%Y %H:%M:%S")
+        super().__init__()
         self.data = []
+        self.seq_name = 'General Constant Current'
         
-class spc_data():
+class SpcData(STMData):
     def __init(self):
-        now = datetime.now()
-        self.time = now.strftime("%d/%m/%Y %H:%M:%S")
+        super().__init__()
         self.data = []
+        self.seq_name = 'SPC default'
--- pyqt5 STM software/Scan/Deposition.py
+++ pyqt5 STM software/Scan/Deposition.py
@@ -13,17 +13,20 @@ sys.path.append("../Model/")
 sys.path.append("../TipApproach/")
 sys.path.append("../Scan/")
 sys.path.append("../Etest/")
-from PyQt5.QtWidgets import QApplication , QWidget
-from PyQt5.QtCore import pyqtSignal , Qt
+from PyQt5.QtWidgets import QApplication , QWidget, QMessageBox, QFileDialog
+from PyQt5.QtCore import pyqtSignal
 from Deposition_ui import Ui_Deposition
 from sequence import mySequence
 import functools as ft
+import conversion as cnv
+from DataStruct import DepData
+import pickle
 
 
 class myDeposition(QWidget, Ui_Deposition):
     close_signal = pyqtSignal()
     seq_list_signal = pyqtSignal()
-    do_it_signal = pyqtSignal()
+    do_it_signal = pyqtSignal(list, list, int)
     stop_signal = pyqtSignal()
     
 
@@ -33,20 +36,25 @@ class myDeposition(QWidget, Ui_Deposition):
         self.init_UI()
 
     def init_UI(self):
+        self.data = DepData()
         self.idling = True
-        self.seq = mySequence([], [], False)
-        self.bias_dac = False
+        self.bias_dac = True
         self.bias_ran = 9
-        self.poke_command = [0x42, 0x63, 0x8d]                                  # Feedback, Shift Z offset, Aout bias
-        self.poke_data = [0x00000000, 0x80000000, 0x00008000]                   # OFF, Forward 0 bit, output zero voltage
-        self.poke_seq = mySequence(self.poke_command, self.poke_data, False)    # Initial poke sequence
+        self.saved = True
+
+        self.seq = mySequence([], [], False)
+        self.poke_seq = mySequence([0x42, 0x63, 0x8d], [0x00000000, 0x80000000, 0x00008000] , False)    # Initial poke sequence
         
         # Connect signal
         self.groupBox_ReadNSample_Deposition.toggled.connect(ft.partial(self.read_mode, True))
         self.groupBox_ReadUntil_Deposition.toggled.connect(ft.partial(self.read_mode, False))
         self.groupBox_Seq_Deposition.toggled.connect(self.seq_mode)
-        self.spinBox_DeltaZ_PokeTip.valueChanged.connect(self.scrollBar_PokeTip.setValue)
-        self.scrollBar_PokeTip.valueChanged.connect(self.spinBox_DeltaZ_PokeTip.setValue)
+        
+        self.spinBox_DeltaZ_PokeTip.valueChanged.connect(self.scrollBar_DeltaZ_PokeTip.setValue)
+        self.scrollBar_DeltaZ_PokeTip.valueChanged.connect(self.spinBox_DeltaZ_PokeTip.setValue)
+        self.spinBox_Bias_PokeTip.valueChanged.connect(ft.partial(self.bias_cnv, True))
+        self.scrollBar_Bias_PokeTip.valueChanged.connect(ft.partial(self.bias_cnv, False))
+        
         self.pushButton_SeqEditor_Deposition.clicked.connect(self.seq_list_signal)
         self.pushButton_DoIt_Deposition.clicked.connect(self.do_it)
         self.pushButton_Save_Deposition.clicked.connect(self.save)
@@ -57,6 +65,14 @@ class myDeposition(QWidget, Ui_Deposition):
         self.enable(succeed)
         self.bias_dac = bias_dac
         self.bias_ran = bias_ran
+        if bias_dac:
+            self.scrollBar_Bias_PokeTip.setMaximum(0xfffff)
+            self.spinBox_Bias_PokeTip.setMaximum(5.0)
+            self.spinBox_Bias_PokeTip.setMinimum(-5.0)
+        else:
+            self.scrollBar_Bias_PokeTip.setMaximum(0xffff)
+            self.bias_ran_change(bias_ran)
+        
         self.seq = seq
         
         self.label_Seq_Deposition.setText(self.eq.name)
@@ -75,30 +91,136 @@ class myDeposition(QWidget, Ui_Deposition):
         self.groupBox_Poke_Deposition.setEnabled(not state)
     
     # Enable serial realted components
-    def enable(self, enable):
+    def enable_serial(self, enable):
         self.groupBox_Seq_Deposition.setEnabled(enable)
         self.groupBox_Read_Deposition.setEnabled(enable)
         self.groupBox_ReadPulse_Deposition.setEnabled(enable)
         self.pushButton_DoIt_Deposition.setEnabled(enable)
-        self.pushButton_Save_Deposition.setEnabled(enable)
+        self.pushButton_Save_Deposition.setEnabled(enable and self.saved)
+        self.pushButton_Save_Deposition.setEnabled(enable and (not self.data.data))
         mode = not self.groupBox_Poke_Deposition.isChecked()
         self.groupBox_Poke_Deposition.setEnabled(enable and mode)
-            
+    
+    # Bias conversion slot
+    def bias_cnv(self, flag, value):
+        if flag:
+            if self.bias_dac:
+                out = cnv.vb(value, '20')
+            else:
+                out = cnv.vb(value, 'd', self.bias_ran)
+            self.scrollBar_Bias_PokeTip.setValue(out)
+        else:
+            if self.bias_dac:
+                out = cnv.bv(value, '20')
+            else:
+                out = cnv.bv(value, 'd', self.bias_ran)
+            self.spinBox_Bias_PokeTip.setValue(out)
+    
     # Do it slot
     def do_it(self):
-        pass
-    
+        if self.idling:
+            # Read before and after
+            if self.groupBox_Read_Deposition.isChecked():
+                read_before_ch = (self.comboBox_Ch_Read.currentIndex() + 6) * 4 + 0xc0
+                read_before_avg = self.spinBox_AvgNum_Read.value()
+                read_before_num = self.spinBox_Num_Read.value()
+                read_before_delay = self.spinBox_Delay_Read.value()
+                read_before = [read_before_ch, read_before_num, read_before_avg, read_before_delay]
+            else:
+                read_before = []
+            
+            # Read pulse
+            read_ch = (self.comboBox_Ch_Pulse.currentIndex() + 6) * 4 + 0xc0
+            read_delay = self.spinBox_Wait_Pulse.value()
+            read_avg = self.spinBox_AvgNum_Pulse.value()
+            read_delay2 = self.spinBox_Delay_Pulse.value()
+            
+            read_num = self.spinBox_Num_ReadNSample.value()
+            
+            read_change = cnv.vb((self.spinBox_Change_ReadUntil.value() - 10.24), 'a')
+            read_stop_num = self.spinBox_StopNum_ReadUntil.value()
+            
+            if not (self.groupBox_ReadNSample_Deposition.isChecked() or self.groupBox_ReadUntil_Deposition.isChecked()):
+                read_mode = 0
+            elif self.groupBox_ReadNSample_Deposition.isChecked():
+                read_mode = 3
+            elif self.radioButton_NSample_ReadUntil.isChecked():
+                read_mode = 2
+            elif self.radioButton_Continuous_ReadUntil.isChecked():
+                read_mode = 1
+            
+            read = [read_ch, read_mode, read_delay, read_delay2, read_num, read_avg, read_change, read_stop_num]
+            self.data = DepData()
+            if read_mode != 0:
+                self.saved = False
+                self.data.read = read
+
+            # Sequence
+            if not self.groupBox_Seq_Deposition.isChecked():
+                poke_command = [0x42, 0x63, 0x8d]                                  # Feedback, Shift Z offset, Aout bias
+                poke_data = [0x00000000, 0x80000000, 0x00008000]                   # OFF, Forward 0 bit, output zero voltage
+                poke_data[2] = self.scrollBar_Bias_PokeTip.value()
+                deltaZ = self.scrollBar_DeltaZ_PokeTip.value()
+                if deltaZ >= 0:
+                    poke_data[1] = 0x80000000 + deltaZ
+                else:
+                    poke_data[1] = 0x80000000 - deltaZ
+            
+                self.poke_seq = mySequence(poke_command, poke_data, False)
+                index = 0
+            else:
+                index = 1
+                
+            if self.saved == True:
+                flag = True
+            else:
+                msg = QMessageBox.information(None, "Depostion", "Data is unsaved. Do you want to continue?", QMessageBox.Ok | QMessageBox.Cancel)
+                if msg == QMessageBox.Ok:
+                    flag = True
+                else:
+                    flag = False
+            if flag:
+                self.do_it_signal.emit(read_before, read, index)
+        else:
+            self.stop_signal.emit()
+                
+    # Bias range change slot
+    def bias_ran_change(self, ran):
+        self.bias_ran = ran
+        if not self.bias_dac:
+            if ran == 9:
+                Max = 5.0
+                Min = -5.0
+            elif ran == 10:
+                Max = 10.0
+                Min = -10.0
+            elif ran == 14:
+                Max = 2.5
+                Min = -2.5
+            self.spinBox_Bias_PokeTip.setMaximum(Max)
+            self.spinBox_Bias_PokeTip.setMinimum(Min)
+            val = self.spinBox_Bias_PokeTip.value()
+            self.bias_cnv(True, val)
+
     # Save data slot
+    # !!! need to know how to figure out file name automatically
     def save(self):
-        pass
+        fname = QFileDialog.getSaveFileName(self, 'Save file', '', 'DEP Files (*.dep)')[0]  # Save file and get file name
+        if fname != '':                         # Savable
+            with open(fname, 'wb') as output:
+                pickle.dump(self.data, output, pickle.HIGHEST_PROTOCOL)         # Save data
+                self.data.path = fname                                          # Save path
+                self.saved = True
+                self.setWindowTitle('Deposition')
     
     # Emit close signal
     def closeEvent(self, event):
-        self.close_signal.emit()
-        event.accept()
+        if self.idling:
+            self.close_signal.emit()
+            event.accept()
+        else:
+            event.ignore()
         
-    def enable_serial(self, enable):
-        pass
 
 
 
