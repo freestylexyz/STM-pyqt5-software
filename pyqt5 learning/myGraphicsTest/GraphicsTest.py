534cc3d81c31bdf6e76ac2fcdbb22e5203829c3e
Dan Bai <35791246+milier00@users.noreply.github.com>
2021-01-15 21:29:13 +0800

    Add files via upload

--- pyqt5 learning/myGraphicsTest/GraphicsTest.py
+++ pyqt5 learning/myGraphicsTest/GraphicsTest.py
@@ -10,7 +10,7 @@ from PyQt5.QtGui import QPixmap, QPen, QCursor
 from PyQt5.QtCore import pyqtSignal, Qt, QRectF, QFileSystemWatcher, QCoreApplication, QDir, QFileInfo, QFile
 from PyQt5 import QtCore
 from GraphicsTest_ui import Ui_GraphicsDemo
-from customROI import CrossCenterROI
+from customROI import *
 import pyqtgraph as pg
 import numpy as np
 import functools as ft
@@ -40,9 +40,14 @@ class myGraphicsDemo(QWidget, Ui_GraphicsDemo):
         self.Point.toggled.connect(ft.partial(self.change_mode, 1))
         self.Rescan.toggled.connect(ft.partial(self.change_mode, 2))
 
+        self.checkBox_Resize_Rescan.toggled.connect(self.enable_resize)
+
         self.pushButton_Send_Default.clicked.connect(self.default_send)
         self.pushButton_Send_Point.clicked.connect(self.point_send)
-        self.pushButton_Send_Rescan.clicked.connect(self.rescan_send)
+
+        self.spinBox_x1_Rescan.editingFinished.connect(lambda: self.rescan_update(0))
+        self.spinBox_y1_Rescan.editingFinished.connect(lambda: self.rescan_update(0))
+        self.spinBox_x3_Rescan.editingFinished.connect(lambda: self.rescan_update(0))
 
         # graphicsView | Scan main view
 
@@ -54,12 +59,21 @@ class myGraphicsDemo(QWidget, Ui_GraphicsDemo):
         self.view_box.setAspectLocked(True)
         self.view_box.setCursor(Qt.CrossCursor)
         self.view_box.setMouseMode(self.view_box.PanMode)
+        # self.view_box.contextMenu = []
 
         # ROI | define pens
         blue_pen = pg.mkPen((70, 200, 255, 255), width=1)
         green_pen = pg.mkPen((150, 220, 0, 255), width=1)
+        purple_pen = pg.mkPen('deaaff', width=1)
         baby_blue_pen = pg.mkPen((130, 220, 255, 255), width=1, dash=[2, 4, 2, 4])
         baby_green_pen = pg.mkPen((210, 255, 120, 255), width=1, dash=[2, 2, 2, 2])
+        serial_pen_0 = pg.mkPen('bee9e8', width=1, dash=[2, 2, 2, 2])
+        serial_pen_1 = pg.mkPen('70d6ff', width=1)
+        serial_pen_2 = pg.mkPen('ff70a6', width=1)
+        serial_pen_3 = pg.mkPen('ff9770', width=1)
+        serial_pen_4 = pg.mkPen('ffd670', width=1)
+        serial_pen_5 = pg.mkPen('e9ff70', width=1)
+        self.serial_pen = [serial_pen_0, serial_pen_1, serial_pen_2, serial_pen_3, serial_pen_4, serial_pen_5]
 
         # ROI | scan area
         self.scan_area = pg.RectROI([0, 0], [300000, 300000], pen=blue_pen, centered=True, \
@@ -93,15 +107,31 @@ class myGraphicsDemo(QWidget, Ui_GraphicsDemo):
         self.tip_position.removeHandle(0)
 
         # ROI | target position
-        self.target_point = pg.CrosshairROI(pos=cross_pos, size=cross_size, pen=baby_green_pen, movable=False, \
+        self.target_Rescan = pg.CrosshairROI(pos=cross_pos, size=cross_size, pen=baby_green_pen, movable=False, \
                                             resizable=False, rotatable=False)
-        self.target_point.setZValue(10)
-        self.view_box.addItem(self.target_point)
-        self.target_point.removeHandle(0)
-        self.target_point.hide()
+        self.target_Rescan.setZValue(10)
+        self.view_box.addItem(self.target_Rescan)
+        self.target_Rescan.removeHandle(0)
+        self.target_Rescan.hide()
 
-        # self.ruler = CrossCenterROI([2000, 2000], [2000000, 2000000])
-        # self.view_box.addItem(self.ruler)
+        # ROI | rescan area
+        self.rescan_area = CrossCenterROI([0, 0], [300000, 300000], pen=purple_pen, centered=True, \
+                                    movable=True, resizable=True, rotatable=False, \
+                                    maxBounds=QRectF(-3276800, -3276800, 6553600, 6553600), scaleSnap=True)
+        self.rescan_area.aspectLocked = True
+        self.view_box.addItem(self.rescan_area)
+        self.rescan_area.removeHandle(0)
+        self.rescan_area.addScaleHandle([0, 0], [1, 1], index=0)
+        self.rescan_area.addScaleHandle([1, 1], [0, 0], index=1)
+        self.rescan_area.hide()
+        self.rescan_area.getHandles()[0].hide()
+        self.rescan_area.getHandles()[1].hide()
+        self.rescan_area.sigRegionChanged.connect(lambda: self.rescan_update(1))
+
+        # ROI | point
+        self.point_0 = pg.PolyLineROI([0, 0], closed=False, pen=self.serial_pen[0])
+        self.view_box.addItem(self.point_0)
+        self.point_0.hide()
 
     def change_mode(self, index, status):
         if status:
@@ -129,30 +159,51 @@ class myGraphicsDemo(QWidget, Ui_GraphicsDemo):
                 self.init_rescan_mode(True)
                 self.init_default_mode(False)
                 self.init_point_mode(False)
+        else:
+            self.init_rescan_mode(False)
+            self.init_default_mode(False)
+            self.init_point_mode(False)
+
 
     def init_default_mode(self, status):
         if status:
             self.scan_area.show()
             self.target_area.show()
             self.tip_position.show()
-            self.target_point.show()
-
+            self.target_Rescan.show()
         else:
             self.scan_area.hide()
             self.target_area.hide()
             self.tip_position.hide()
-            self.target_point.hide()
-
-
-
+            self.target_Rescan.hide()
 
     def init_point_mode(self, status):
         if status:
-            pass
+            self.point_0.show()
+        else:
+            self.point_0.hide()
+
+    def enable_resize(self, status):
+        if status:
+            self.rescan_area.getHandles()[0].show()
+            self.rescan_area.getHandles()[1].show()
+        else:
+            self.rescan_area.getHandles()[0].hide()
+            self.rescan_area.getHandles()[1].hide()
 
     def init_rescan_mode(self, status):
         if status:
-            pass
+            self.rescan_area.show()
+            self.enable_resize(self.checkBox_Resize_Rescan.isChecked())
+            x1 = int(self.rescan_area.pos()[0])
+            y1 = int(self.rescan_area.pos()[1])
+            x3 = int(self.rescan_area.getHandles()[1].pos()[0])
+            self.spinBox_x1_Rescan.setValue(x1)
+            self.spinBox_y1_Rescan.setValue(y1)
+            self.spinBox_x3_Rescan.setValue(x3)
+        else:
+            self.rescan_area.hide()
+            self.enable_resize(self.checkBox_Resize_Rescan.isChecked())
 
     def default_send(self):
         x1 = self.spinBox_x1_Default.value()
@@ -174,8 +225,23 @@ class myGraphicsDemo(QWidget, Ui_GraphicsDemo):
     def point_update(self):
         pass
 
-    def rescan_update(self):
-        pass
+    def rescan_update(self, index):
+        if index == 0:
+            self.enable_resize(self.checkBox_Resize_Rescan.isChecked())
+            x1 = self.spinBox_x1_Rescan.value()
+            y1 = self.spinBox_y1_Rescan.value()
+            x3 = self.spinBox_x3_Rescan.value()
+            self.rescan_area.movePoint(self.rescan_area.getHandles()[0], [x1, y1], finish=False)
+            self.rescan_area.movePoint(self.rescan_area.getHandles()[1], [x3 + x1, x3 + y1])
+            self.rescan_area.setPos([x1, y1])
+        elif index == 1:
+            self.enable_resize(self.checkBox_Resize_Rescan.isChecked())
+            x1 = int(self.rescan_area.pos()[0])
+            y1 = int(self.rescan_area.pos()[1])
+            x3 = int(self.rescan_area.getHandles()[1].pos()[0])
+            self.spinBox_x1_Rescan.setValue(x1)
+            self.spinBox_y1_Rescan.setValue(y1)
+            self.spinBox_x3_Rescan.setValue(x3)
 
     # tool bar button slot | Full view
     def full_view(self):
--- pyqt5 learning/myGraphicsTest/customROI.py
+++ pyqt5 learning/myGraphicsTest/customROI.py
@@ -44,3 +44,28 @@ class CrossCenterROI(ROI):
         p.drawRect(0, 0, 20, 20)                  # Dan 1/4/2021
         p.drawLine(Point(10, 9), Point(10, 11))  # Dan 1/4/2021
         p.drawLine(Point(9, 10), Point(11, 10))  # Dan 1/4/2021
+
+
+class PointROI(ROI):
+    def __init__(self, pos, size, centered=False, sideScalers=False, **args):
+        ROI.__init__(self, pos, size, **args)
+        if centered:
+            center = [0.5, 0.5]
+        else:
+            center = [0, 0]
+
+        self.addScaleHandle([1, 1], center)
+        if sideScalers:
+            self.addScaleHandle([1, 0.5], [center[0], 0.5])
+            self.addScaleHandle([0.5, 1], [0.5, center[1]])
+
+    def paint(self, p, opt, widget):
+        # Note: don't use self.boundingRect here, because subclasses may need to redefine it.
+        r = QtCore.QRectF(0, 0, self.state['size'][0], self.state['size'][1]).normalized()
+        p.setRenderHint(QtGui.QPainter.Antialiasing)
+        p.setPen(self.currentPen)
+        p.translate(r.left(), r.top())
+        p.scale(r.width() / 20, r.height() / 20)  # Dan 1/4/2021
+        p.drawRect(0, 0, 20, 20)                  # Dan 1/4/2021
+        p.drawLine(Point(10, 9), Point(10, 11))  # Dan 1/4/2021
+        p.drawLine(Point(9, 10), Point(11, 10))  # Dan 1/4/2021
